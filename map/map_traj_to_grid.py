7020fb7c0df952e99e43e378e7b180a4162e1d1f
yuzisheng <yuzisheng@jd.com>
2019-04-27 16:28:06 +0800

    fix

--- map/map_traj_to_grid.py
+++ map/map_traj_to_grid.py
@@ -9,11 +9,11 @@ import seaborn as sns
 
 
 class MapTrajToGrid:
-    def __init__(self, traj, time_idx, lat_idx, lng_idx):
-        self.traj = traj
-        self.time_idx = time_idx
-        self.lat_idx = lat_idx
-        self.lng_idx = lng_idx
+    def __init__(self):
+        self.traj = None
+        self.time_idx = None
+        self.lat_idx = None
+        self.lng_idx = None
 
     def transfer_coord_gcj02_to_wgs84(self):
         """
@@ -25,19 +25,27 @@ class MapTrajToGrid:
             self.traj[i][self.lng_idx] = lnglat_wgs[0]
             self.traj[i][self.lat_idx] = lnglat_wgs[1]
         pd.DataFrame(data=self.traj, columns=None).to_csv("../data/traj_wgs.csv", index=False)
-        print("coord gcj02 to wgs84 done")
+        print("++ coord gcj02 to wgs84 done")
 
-    def map_traj_to_grid(self, min_lat, min_lng, max_lat, max_lng, grid_size):
+    def map_traj_to_grid(self, traj, time_idx, lat_idx, lng_idx, mbr, grid_size, if_wgs=True):
         """
         map traj to grid
-        :param min_lat:
-        :param min_lng:
-        :param max_lat:
-        :param max_lng:
-        :param grid_size:
+        :param traj: .npy格式的轨迹数据
+        :param time_idx: traj中时间属性的索引
+        :param lat_idx: traj中纬度属性的索引
+        :param lng_idx: traj中经度属性的索引
+        :param mbr: 轨迹的Bounding Box
+        :param grid_size: 需划分的网格大小
+        :param if_wgs: 轨迹坐标是否为wgs84坐标系
         :return:
         """
-        mbr = MBR(min_lat, min_lng, max_lat, max_lng)
+        self.traj = traj
+        self.time_idx = time_idx
+        self.lat_idx = lat_idx
+        self.lng_idx = lng_idx
+        # 若非wgs84坐标系,目前默认为gcj02,并转为wgs84
+        if not if_wgs:
+            self.transfer_coord_gcj02_to_wgs84()
         grid = Grid(mbr, grid_size, grid_size)
         res = np.zeros((grid_size, grid_size), dtype=np.int32)
         for i in range(len(self.traj)):
@@ -47,20 +55,21 @@ class MapTrajToGrid:
                 row_idx, col_idx = grid.get_matrix_idx(lat, lng)
                 res[row_idx, col_idx] += 1
             except IndexError:
-                print('({0}, {1}) is out of mbr, skip'.format(lat, lng))
+                # print('({0}, {1}) is out of mbr, skip'.format(lat, lng))
                 continue
         np.save("../data/map_traj_to_grid.npy", res)
-        print("map traj to grid is done")
+        print("++ map traj to grid is done")
         return res
 
 
 if __name__ == '__main__':
     # load data
-    data = pd.read_csv("../data/TaxiguiAU0676.csv", header=None).values
-    map_traj_to_map = MapTrajToGrid(data, time_idx=0, lat_idx=2, lng_idx=3)
-    map_traj_to_map.transfer_coord_gcj02_to_wgs84()
-    arr = map_traj_to_map.map_traj_to_grid(min_lat=26.337, min_lng=106.389,
-                                           max_lat=26.788, max_lng=106.966, grid_size=128)
+    gy_taxi = pd.read_csv("../data/TaxiguiAU0676.csv", header=None).values
+    # create mbr
+    gy_mbr = MBR(26.548, 106.584, 26.704, 106.807)
+    # create map_traj_to_map instance
+    map_traj_to_map = MapTrajToGrid()
+    arr = map_traj_to_map.map_traj_to_grid(gy_taxi, 0, 2, 3, gy_mbr, 128, if_wgs=False)
     # draw heatmap
     sns.set()
     sns.heatmap(arr, square=True)
